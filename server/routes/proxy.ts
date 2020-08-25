59c75baf6525ad501ef4a1549cb43fc6a7da8174
phatpham9 <phatpham9@gmail.com>
2020-08-25 17:16:18 +0700

    refactor(server): update http proxy middleware implementation

--- server/routes/proxy.ts
+++ server/routes/proxy.ts
@@ -3,19 +3,13 @@ import * as proxyMiddleware from 'http-proxy-middleware';
 
 const { API_URL } = process.env;
 
-const config = {
-  '/api': {
-    target: API_URL,
-    pathRewrite: { '^/api': '/' },
-  },
-};
-
 const routes = () => {
   const router = Router();
 
-  Object.keys(config).forEach((context: any) => {
-    router.use(proxyMiddleware(context, config[context]));
-  });
+  router.use('/api', proxyMiddleware({
+    target: API_URL,
+    pathRewrite: { '^/api': '/' },
+  }));
 
   return router;
 };
